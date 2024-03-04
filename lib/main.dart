2a043bfc02393d3ab7667611dcb417e0e93e28da
Glenn Turla <turlaglenn@gmail.com>
2024-03-05 01:57:52 +0800

    still editing the dashboard for the main page

--- lib/main.dart
+++ lib/main.dart
@@ -61,91 +61,173 @@ class MyHomePage extends StatelessWidget {
               ),
             ),
             ListTile(
-              title: Text(
-                'Item 1',
-                style: TextStyle(
-                  color: Colors.black,
+              leading: Icon(Icons.person),
+              title: GestureDetector(
+                onTap: () {
+                  // Add your logic here
+                },
+                child: Text(
+                  'Glenn Turla',
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
                 ),
               ),
-              onTap: () {
-                // Add your logic here
-              },
             ),
             ListTile(
-              title: Text(
-                'Item 2',
-                style: TextStyle(
-                  color: Colors.black,
+              leading: Icon(Icons.person),
+              title: GestureDetector(
+                onTap: () {
+                  // Add your logic here
+                },
+                child: Text(
+                  'Rena Simbol',
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
                 ),
               ),
-              onTap: () {
-                // Add your logic here
-              },
             ),
             ListTile(
-              title: Text(
-                'Item 3',
-                style: TextStyle(
-                  color: Colors.black,
+              leading: Icon(Icons.person),
+              title: GestureDetector(
+                onTap: () {
+                  // Add your logic here
+                },
+                child: Text(
+                  'Jasper Ingal',
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
                 ),
               ),
-              onTap: () {
-                // Add your logic here
-              },
+            ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: GestureDetector(
+                onTap: () {
+                  // Add your logic here
+                },
+                child: Text(
+                  'Rustine Dullas',
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: GestureDetector(
+                onTap: () {
+                  // Add your logic here
+                },
+                child: Text(
+                  'Marc Turla',
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ),
+            Divider(), // Divider for visual separation
+            ListTile(
+              leading: Icon(Icons.info),
+              title: ListTile(
+                onTap: () {
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        title: Text("Our Team"),
+                        content: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text("Glenn Daniel M. Turla"),
+                            Text("Rena Bianca R. Simbol"),
+                            Text("Jasper O. Ingal"),
+                            Text("Rustine A. Dullas"),
+                            Text("Marc Emerson B. Turla"),
+                          ],
+                        ),
+                        actions: <Widget>[
+                          TextButton(
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: Text("Close"),
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                },
+                title: Text(
+                  'About Us',
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              ),
             ),
           ],
         ),
       ),
-      body: Padding(
-        padding: EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            CarouselSlider(
-              options: CarouselOptions(
-                height: 200.0,
-                enlargeCenterPage: true,
-                autoPlay: true,
-                aspectRatio: 16 / 9,
-                autoPlayCurve: Curves.fastOutSlowIn,
-                enableInfiniteScroll: true,
-                autoPlayAnimationDuration: Duration(milliseconds: 800),
-                viewportFraction: 0.8,
-              ),
-              items: imageLinks.map((String link) {
-                return Container(
-                  margin: EdgeInsets.all(5.0),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8.0),
-                    image: DecorationImage(
-                      image: NetworkImage(link),
-                      fit: BoxFit.cover,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              CarouselSlider(
+                options: CarouselOptions(
+                  height: 200.0,
+                  enlargeCenterPage: true,
+                  autoPlay: true,
+                  aspectRatio: 16 / 9,
+                  autoPlayCurve: Curves.fastOutSlowIn,
+                  enableInfiniteScroll: true,
+                  autoPlayAnimationDuration: Duration(milliseconds: 800),
+                  viewportFraction: 0.8,
+                ),
+                items: imageLinks.map((String link) {
+                  return Container(
+                    margin: EdgeInsets.all(5.0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8.0),
+                      image: DecorationImage(
+                        image: NetworkImage(link),
+                        fit: BoxFit.cover,
+                      ),
                     ),
-                  ),
-                );
-              }).toList(),
-            ),
-            SizedBox(height: 20.0),
-            Text(
-              'Welcome to our Portfolio!',
-              style: TextStyle(
-                fontSize: 24.0,
-                fontWeight: FontWeight.bold,
+                  );
+                }).toList(),
               ),
-            ),
-            SizedBox(height: 20.0),
-            Text(
-              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis libero et nisi scelerisque condimentum. Aenean ultrices, libero eget egestas aliquam, felis urna cursus enim, in ultricies purus odio eget justo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(height: 20.0),
-            ElevatedButton(
-              onPressed: () {
-                // Add your logic here
-              },
-              child: Text('View Portfolio'),
-            ),
-          ],
+              SizedBox(height: 20.0),
+              Text(
+                'Welcome to our Portfolio!',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 20.0),
+              Text(
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis libero et nisi scelerisque condimentum. Aenean ultrices, libero eget egestas aliquam, felis urna cursus enim, in ultricies purus odio eget justo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 20.0),
+              ElevatedButton(
+                onPressed: () {
+                  // Add your logic here
+                },
+                child: Text('View Portfolio'),
+              ),
+            ],
+          ),
         ),
       ),
     );
